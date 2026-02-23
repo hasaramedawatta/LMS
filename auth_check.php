055c2fb8197a09447e192b60c2e294327aed2514
Hasara Sewwandi Medawatta <hasaramedawatta@gmail.com>
2026-02-23 16:23:06 +0530

    delete

--- auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("../config/db.php");
-
-if(!isset($_SESSION['user'])){
-    header("Location: ../auth/login.php");
-    exit();
-}
-?>
--- footer.php
+++
@@ -1,9 +0,0 @@
-</div> <!-- end main-content -->
-</div> <!-- end layout -->
-
-<footer class="footer">
-    <p>© <?php echo date("Y"); ?> Simple LMS | First Year Project</p>
-</footer>
-
-</body>
-</html>
